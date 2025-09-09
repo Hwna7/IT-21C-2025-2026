9c235dab33410de9f6a3d0d42d6ad3f14acbd3cb
ohwen <ohwentudtuda7@gmail.com>
2025-09-09 15:04:08 +0800

    Added operators for daily routine

---
+++ TudtudLife.js
@@ -0,0 +1,9 @@
+let name = "Mark ohwen R. Tudtud"
+let course = "Bachelor of Science in Information Technology"
+let year = "2nd Year";
+
+console.log("Name: "+ name);
+console.log("Course: "+ course);
+console.log("Year Level: "+ year);
+
+let 
