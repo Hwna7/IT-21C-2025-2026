aa4f469c22d1d79c4aa1780473ab72326fb06a57
ohwen <ohwentudtuda7@gmail.com>
2025-09-09 15:06:30 +0800

    Added condition for study hours

--- TudtudLife.js
+++ TudtudLife.js
@@ -6,4 +6,17 @@ console.log("Name: "+ name);
 console.log("Course: "+ course);
 console.log("Year Level: "+ year);
 
-let 
+let sleep = 8
+let study = 2
+
+console.log("Total hours of sleep: "+ sleep);
+console.log("Total hours of study: "+ study);
+
+console.log("Total hours of sleep: "+ sleep);
+console.log("Total hours of study: "+ study);
+
+if (study>5) {
+    console.log("Great! You studied enough today.");
+} else {
+    console.log("You need to study more.");
+}
